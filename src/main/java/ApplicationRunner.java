47d6053f75df14ce7500e6e891fe53e81a3db284
Igor Novokshonov <gogainda@yandex.ru>
2017-01-18 02:45:47 +0200

    Initital commit

---
+++ src/main/java/ApplicationRunner.java
@@ -0,0 +1,16 @@
+/**
+ * Created by igor on 2017-01-18.
+ */
+public class ApplicationRunner {
+    public void startBiddingIn(FakeAuctionServer auction) {
+
+    }
+
+    public void showsSniperHasLostAuction() {
+
+    }
+
+    public void stop() {
+
+    }
+}
---
+++ src/main/java/FakeAuctionServer.java
@@ -0,0 +1,25 @@
+/**
+ * Created by igor on 2017-01-18.
+ */
+public class FakeAuctionServer {
+    public FakeAuctionServer(String s) {
+
+    }
+
+    public void startSellingItem() {
+
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+
+
+    }
+
+    public void announceClosed() {
+
+    }
+
+    public void stop() {
+
+    }
+}
