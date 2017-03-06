3ff77c65ec5683ac7f37bf167e6133d8b0ca3361
Igor Novokshonov <gogainda@yandex.ru>
2017-03-06 04:04:37 +0300

    Introduced stop price, fix one test, many to go

--- src/test/java/auctionsniper/ui/MainWindowTest.java
+++ src/test/java/auctionsniper/ui/MainWindowTest.java
@@ -1,6 +1,9 @@
 package auctionsniper.ui;
 
 import auctionsniper.AuctionSniperDriver;
+import auctionsniper.Item;
+import auctionsniper.SniperPortfolio;
+import auctionsniper.UserRequestListener;
 import com.objogate.wl.swing.probe.ValueMatcherProbe;
 import org.junit.Test;
 
@@ -8,21 +11,22 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class MainWindowTest {
     private final SnipersTableModel tableModel = new SnipersTableModel();
-//    private final MainWindow mainWindow = new MainWindow(tableModel);
+    private final SniperPortfolio portfolio = new SniperPortfolio(tableModel);
+    private final MainWindow mainWindow = new MainWindow(portfolio);
     private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
     @Test
     public void
     makesUserRequestWhenJoinButtonClicked() {
-        System.setProperty("com.objogate.wl.keyboard", "US");
-        final ValueMatcherProbe<String> buttonProbe =
-                new ValueMatcherProbe<String>(equalTo("an item-id"), "join request");
-//        mainWindow.addUserRequestListener(
-//                new UserRequestListener() {
-//                    public void joinAuction(String itemId) {
-//                        buttonProbe.setReceivedValue(itemId);
-//                    }
-//                });
-        driver.startBiddingFor("an item-id");
-        driver.check(buttonProbe);
+        final ValueMatcherProbe<Item> itemProbe =
+                new ValueMatcherProbe<Item>(equalTo(new Item("an item-id", 789)), "item request");
+        mainWindow.addUserRequestListener(
+                new UserRequestListener() {
+                    public void joinAuction(Item item) {
+                        System.out.println(item);
+                        itemProbe.setReceivedValue(item);
+                    }
+                });
+        driver.startBiddingFor("an item-id", 789);
+        driver.check(itemProbe);
     }
 }
